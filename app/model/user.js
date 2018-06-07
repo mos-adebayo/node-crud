4c53fd7f8b2f2cf007d9884258ba2757d633ad81
moses.adebayo <moses.adebayo@venturegardengroup.com>
2018-06-07 11:13:16 +0100

    salutation method added to user schema

--- app/model/user.js
+++ app/model/user.js
@@ -4,13 +4,24 @@ var Schema = mongoose.Schema;
 //Create a Schema
 var userSchema = new Schema({
     name: String,
+    salutation: String,
+    gender: {type: String, enum: ['male', 'female'], default: 'male'},
     username: {type: String, requires: true, unique: true},
     password: { type: String, required: true},
     position: String,
+    maritalStatus: {type: String, enum: ['single', 'married'], default: 'single'},
     created_at: Date,
     modified_at: Date
 });
 
+//custom method for schema
+userSchema.methods.salute = function () {
+    if(this.gender === 'male'){
+        return 'Mr.'
+    }else{
+        return (this.maritalStatus === 'single' ? 'Miss' : 'Mrs.');
+    }
+};
 
 //Create a model for the user
 var User = mongoose.model('User', userSchema);
